83c57b4e846cc46b1ab909d1edbe8c640215ec07
Brady Mitchelmore <Bradymitchelmore@gmail.com>
2023-06-17 16:19:17 -0230

    can now group nodes together, no more overlapping with default geometry

--- canvas.py
+++ canvas.py
@@ -4,19 +4,93 @@ import json
 class Node:
     def __init__(self, node_type, id, x=0, y=0, width=0, height=0, color="", label="", filename="", filepath="", text=""):
         self.node = {"type": node_type, "id": id, "x": x, "y": y, "width": width, "height": height, "color": color}
-        node_items = list(self.node.items())
+
+        if node_type == "file":
+            self.node["file"] = filename
+
+        if node_type == "text":
+            self.node["text"] = text
 
         if node_type == "group":
-            node_items.insert(1, ("label", label))
-        elif node_type == "file":
-            # save file to filepath
-            with open(filepath, 'w') as f:
-                f.write(text)
-            node_items.insert(1, ("file", filename))
-        elif node_type == "text":
-            node_items.insert(1, ("text", text))
+            self.node["label"] = label
+    
+    def _get_cords(self, node_map, padding=20, position="right"):
+        x_min = 0
+        x_max = 0
+        y_min = 0
+        y_max = 0
+        for node in node_map.values():
+            if node.node["x"] < x_min:
+                x_min = node.node["x"]
+            if node.node["x"] + node.node["width"] > x_max:
+                x_max = node.node["x"] + node.node["width"]
+            if node.node["y"] < y_min:
+                y_min = node.node["y"]
+            if node.node["y"] + node.node["height"] > y_max:
+                y_max = node.node["y"] + node.node["height"]
 
-        self.node = dict(node_items)
+        if position == "left":
+            x_min -= padding
+            return x_min, y_min
+        elif position == "top":
+            y_min -= padding
+            return x_min, y_min
+        elif position == "bottom":
+            y_max += padding
+            return x_min, y_max
+        else:
+            x_max += padding
+            return x_max, y_min
+
+
+class GroupNode(Node):
+    def __init__(self, id, node_map, children=None, color="", label=""):
+        super().__init__("group", id=id, color=color, label=label)
+        self.node["x"], self.node["y"], self.node["width"], self.node["height"] = self._get_cords(node_map, children)
+
+    def _get_cords(self, node_map, children, alignment="h-line"):
+        x = 0
+        y = 0
+        width = 0
+        height = 0
+        if children is None:
+            print("No children")
+            return x, y, width, height
+        for child_id in children:
+            child = node_map[child_id]
+            if not child.node["id"] in node_map:
+                raise ValueError("Child ID not in node map")
+            if child.node["x"] < x:
+                x = child.node["x"]
+            if child.node["y"] < y:
+                y = child.node["y"]
+            if alignment == "h-line":
+                width += child.node["width"]
+                if child.node["height"] > height:
+                    height = child.node["height"]
+            elif alignment == "v-line":
+                height += child.node["height"]
+                if child.node["width"] > width:
+                    width = child.node["width"]
+        return x, y, width, height
+
+
+class FileNode(Node):
+    def __init__(self, id, node_map, filename, filepath, text, width, height, color):
+        super().__init__("file", id, width=width, height=height, filename=filename, filepath=filepath, text=text, color=color)
+        self.node["x"], self.node["y"] = self._get_cords(node_map)
+        self._save_file(filepath, text)
+
+    def _save_file(self, filepath, text):
+        with open(filepath, 'w') as f:
+            f.write(text)
+
+
+class TextNode(Node):
+    def __init__(self, id, node_map, text, width, height, color):
+        super().__init__("text", id, width=width, height=height, text=text, color=color)
+        self.node["x"], self.node["y"] = self._get_cords(node_map)
+
 
 class Edge:
     def __init__(self, edge_type, id, fromNode, fromSide, toNode, toSide, toEnd=None, fromEnd=None, color=""):
@@ -36,9 +110,15 @@ class Canvas:
     def _generate_id(self):
         return uuid.uuid4().hex
 
-    def add_node(self, node_type, filename="", text="", x=0, y=0, width=0, height=0, color="", label=""):
+    def add_node(self, node_type, children=None, filename="", text="", x=0, y=0, width=100, height=100, color="", label=""):
         id = self._generate_id()
-        node = Node(node_type, id, x, y, width, height, color, label, filepath=self.root_dir + filename, filename=filename, text=text)
+        if node_type == "file":
+            node = FileNode(id=id, node_map=self.node_map, filename=filename, filepath=self.root_dir + filename, text=text, width=width, height=height, color=color)
+        elif node_type == "group":
+            node = GroupNode(node_map=self.node_map, children=children, id=id, color=color, label=label)
+        else:
+            node = TextNode(id=id, node_map=self.node_map, text=text, width=width, height=height, color=color)
+        # node = Node(node_type, id, x, y, width, height, color, label, filepath=self.root_dir + filename, filename=filename, text=text)
         self.node_map[id] = node
         self.canvas["nodes"].append(node.node)
         return id
--- main.py
+++ main.py
@@ -8,12 +8,16 @@ path_to_vault = "/Users/bradymitchelmore/Library/Mobile Documents/iCloud~md~obsi
 
 # create a new canvas file and save it to the vault
 canvas = Canvas(path_to_vault)
-node1_id = canvas.add_node(node_type='text', text='Hello World!', x=100, y=100, width=100, height=100)
-node2_id = canvas.add_node(node_type='text', text='Hello World!', x=150, y=150, width=100, height=100)
-node3_id = canvas.add_node(node_type='file', filename="test.md", text='testing', x=200, y=200, width=100, height=100)
+node1_id = canvas.add_node(node_type='text', text='Hello World!')
+node2_id = canvas.add_node(node_type='text', text='Hello World!')
+node3_id = canvas.add_node(node_type='file', filename="test.md", text='testing', width=200, height=200)
 
 # Now you can use these IDs to create an edge
 canvas.add_edge('directional', node1_id, 'right', node2_id, 'left')
+canvas.add_edge('directional', node1_id, 'right', node3_id, 'left')
+
+# create a group node
+group_id = canvas.add_node(node_type='group', children=[node1_id, node2_id], label='group')
 
 # Save the canvas to a file
 canvas.save_to_file(f'test.canvas')
